1c094897fd37d131b4be57c3d31ae7ff1019afd5
Sivert Stavland <stavland.sivert@gmail.com>
2018-09-25 15:33:27 +0200

    Added Product entity

---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,22 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "categori")
+	private String categori;
+
+	@Column(name = "name")
+	private String name;
+
+}
