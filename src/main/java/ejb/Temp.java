33423dc6ffff1b54703867fdf809021f22c9dca3
Kevin294 <kevin.arnesen@hotmail.com>
2018-09-25 16:48:27 +0200

    rss files

--- src/main/java/ejb/Temp.java
+++ src/main/java/ejb/Temp.java
@@ -15,10 +15,25 @@ public class Temp {
 	@PostConstruct
 	private void createData() {
 		Address address = new Address("Norway", "Bergen", "Nordåsvegen 66", 5235);
-		Auction auction = new Auction(bid, product, "Kevin294");
 		Bid bid = new Bid(new Double(145.55), "Kevin294");
 		Feedback feedback = new Feedback(4.5,"Kevin294","This dude is the real shit" );
 		Product product = new Product("Titanium Rolex 2017 Model F", "Accessory", "Watch");
 		User user = new User("Kevin294", "Kevin", "Arnesen", address, "kevin@gmail.com", 12345678);
+		Auction auction = new Auction(bid, product, "Kevin294");
+
+		dao.persist(address);
+		dao.persist(bid);
+		dao.persist(feedback);
+		dao.persist(product);
+		dao.persist(user);
+		dao.persist(auction);
+		
+		System.out.println(dao.getAll("address"));
+		System.out.println(dao.getAll("bid"));
+		System.out.println(dao.getAll("feedback"));
+		System.out.println(dao.getAll("product"));
+		System.out.println(dao.getAll("user"));
+		System.out.println(dao.getAll("auction"));
+
 	}
 }
