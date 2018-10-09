1f8a9117cc41e7116a2289e505340e278f4bb6b5
Sivert Stavland <stavland.sivert@gmail.com>
2018-10-09 14:29:11 +0200

    dsas

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -13,7 +13,7 @@ public class Product implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id")
+	@Column(name = "productId")
 	private Integer id;
 
 	@Column(name = "description")
